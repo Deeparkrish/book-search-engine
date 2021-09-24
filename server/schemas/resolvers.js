be46997a26eab8995b2b8a09cbf4c0f3f26920f1
Deeparkrish <kosalai17@gmail.com>
2021-09-24 10:45:53 -0700

    adding context

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,14 +5,18 @@ const { signToken } = require('../utils/auth');
 
 const resolvers ={
     Query :{
-        me: async (parent, args) => {
+        me: async (parent, args,context) => {
+            if(context.user){
             const userData = await User.findOne({})
               .select('-__v -password')
               .populate('savedBooks');
 
               return userData;
 
-          },
+          }
+          throw new AuthenticationError('Not logged in');
+
+        }
         
     },
     Mutation:{
